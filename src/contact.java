763bf9ef71e28e76402fa91c423804d1e63f1452
Chase Martinez <chasemartinez22@gmail.com>
2022-06-20 16:56:27 -0500

    Adding extra contact class to test number manipulation

---
+++ src/contact.java
@@ -0,0 +1,33 @@
+import com.sun.jdi.IntegerValue;
+
+public class contact {
+    private String phoneNumber;
+    private String name;
+
+    public contact(){
+    }
+
+//    public contact(String phoneNumber, String name){
+//        this.phoneNumber = phoneNumber;
+//        this.name = name;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        String number = "2101111111";
+//        int intNumber = Integer.parseInt(number);
+//        String newNumber = null;
+//        if (phoneNumber.length() == 10){
+//            int count = 0;
+//            for(int i = 0;i < number.length();i++){
+//                if(count == 4){
+//
+//                } else if (count == 3){
+//
+//                } else {
+//                    newNumber+=
+//                }
+//                count++;
+//            }
+//        }
+//    }
+}
